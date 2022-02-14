dc658fb406d8ecf7e959c9286f05eaf6de4e2594
Rana Katebi <rana_katebi@hotmail.com>
2022-02-14 13:28:48 +0300

    React Weather App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="weather-app">
+          <form className="search-form">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Type a city.."
+                  autofocus="on"
+                  autocomplete="off"
+                  className="form-control shadow-sm"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="form-control btn btn-primary shadow-sm"
+                />
+              </div>
+            </div>
+          </form>
+          <h1>Sydney NSW, Australia</h1>
+          <ul>
+            <li>Wednesday 22:00</li>
+            <li>Partly Cloudy</li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix">
+                <img
+                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                  alt="Cloudy icon"
+                  className="weather-icon float-left"
+                />
+                <div className="float-left">
+                  <span className="temperature">19</span>
+                  <span className="units">
+                    °C | <a href="#">°F</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Precipitation: 0%</li>
+                <li>Humidity: 77%</li>
+                <li>Wind: 8 km/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
