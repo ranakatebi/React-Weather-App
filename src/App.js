ca90cedb6623c951e073bc8e63ac55eef754a68a
Rana Katebi <rana_katebi@hotmail.com>
2022-02-19 22:29:08 +0300

    add whether css and js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,39 @@
 import React from "react";
-//weather app
+import Weather from "./Weather";
+import "./App.css";
+
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="weather-app">
-          <form className="search-form">
-            <div className="row">
-              <div className="col-9">
-                <input
-                  type="search"
-                  placeholder="Type a city.."
-                  autoFocus="on"
-                  autoComplete="off"
-                  className="form-control shadow-sm"
-                />
-              </div>
-              <div className="col-3">
-                <input
-                  type="submit"
-                  value="Search"
-                  className="form-control btn btn-primary shadow-sm"
-                />
-              </div>
-            </div>
-          </form>
-          <h1>Sydney NSW, Australia</h1>
-          <ul>
-            <li>Wednesday 22:00</li>
-            <li>Partly Cloudy</li>
-          </ul>
-          <div className="row">
-            <div className="col-6">
-              <div className="clearfix">
-                <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                  alt="Cloudy icon"
-                  className="weather-icon float-left"
-                />
-                <div className="float-left">
-                  <span className="temperature">19</span>
-                  <span className="units">
-                    °C | <a href="#top">°F</a>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>Precipitation: 0%</li>
-                <li>Humidity: 77%</li>
-                <li>Wind: 8 km/h</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.delac.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Matt Delac
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
