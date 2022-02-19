cac4ca33010ba19fc3a88c02aa23a0f2df6e81cb
Rana Katebi <rana_katebi@hotmail.com>
2022-02-19 23:12:25 +0300

    add github link

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
           </a>{" "}
           and is{" "}
           <a
-            href="https://github.com/wecodeschool/react-weather-app"
+            href="https://github.com/ranakatebi/React-Weather-App"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -27,7 +27,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
+            href="https://ranareactweather.netlify.app/"
             target="_blank"
             rel="noopener noreferrer"
           >
